43ddc353725696bbf31ded453d8c41f8f0212d0f
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2024-07-21 05:19:22 +0700

    Stable working backup

--- FUKFUK.py
+++ FUKFUK.py
@@ -6,34 +6,54 @@ import traceback
 def search_in_excel(file_path, search_string):
     try:
         df = pd.read_excel(file_path, engine='openpyxl')
+        #If over 66% of a column is NaN, drop it
+        df = df.dropna (axis = 1, thresh = int(df.shape[0]*0.66))
+        
         # Convert all columns to string for easier searching
         df = df.map (str)
-        avg_lengths = df.apply(lambda x: x.str.len().mean())
-        # Find the column with the maximum average length
-        max_avg_column = avg_lengths.idxmax()
-        
-        # Iterate over each row and search for the string
-        
+        avg_lengths = df.apply(lambda x: x.str.len().sum())
+        # Find the column with the maximum sum length
+        max_avg_column = avg_lengths.idxmax()        
         col = max_avg_column
-        #for index, row in df.iterrows():
-            #if search_string in row[col]:
-            #    return [row.tolist()], [index], [1.01]
-            #else:
-                # Use fuzzy matching to find close matches
+
+        # Use fuzzy matching to find close matches
         close_matches = process.extract(search_string, df[col], limit=10)
         result = []
         for match in close_matches:
-            if (match[1] >= threshold) or len(result)<5:
-                result.append (match)
-                #best_match, score, wtf = close_matches[0]
-                #if score > 85:  # Adjust the similarity threshold as needed
-                #    return row.tolist(), index, f"Close match found in '{col}' column: '{best_match}' with score {score}"
+            if (match[1] >= threshold) or (len(result)<5 and (match[1] >= threshold*0.8)):
+                nigga_string = df.iloc[match[2]].to_string(header=False, index=False)
+
+                #remove tabs and spaces
+                cleaned_string = ' '.join(nigga_string.split())
+                #remove date format
+                if cleaned_string.find ("00:00:00") != -1:
+                    cleaned_string = cleaned_string [cleaned_string.find ("00:00:00")+9:]
+                    
+                result.append ([cleaned_string, match[1]])
+                #result.append ([, match[1]])                
         return result, max_avg_column
     except Exception as e:
         traceback.print_exc()
         print(f"Error reading {file_path}: {e}")
         return None, None
 
+def get_all_file (directory, file_extension = "xlsx", depth_max = 3):
+    def search(directory, current_depth):
+        gay = []
+        if current_depth > depth_max:
+            return gay
+        try:
+            for entry in os.scandir(directory):
+                if entry.is_dir(follow_symlinks=False):
+                    nigger = search(entry.path, current_depth + 1)
+                    gay = gay + nigger
+                elif entry.is_file() and entry.name.endswith('.'+file_extension):
+                    gay.append (entry.path)
+            return gay
+        except PermissionError:
+            print ("Permission Error at {directory}, skipping...")
+            return gay
+    return search (directory, 0)
 # Function to search in all Excel files in a directory
 def search_in_directory(directory, search_string):
     results = []
@@ -41,13 +61,12 @@ def search_in_directory(directory, search_string):
         if filename.endswith(".xlsx"):
             file_path = os.path.join(directory, filename)
             result, column_idx = search_in_excel(file_path, search_string)
-            print (column_idx,' ayyyy')
             if result is not None:
                 results.append((filename, result, column_idx))
     return results
 
-threshold = 85
-directory_path = 'C:/Users/za/Desktop/'
+threshold = 90
+directory_path = './Data'
 def wrapper_function (search_string, directory_path = directory_path):
     search_results = search_in_directory(directory_path, search_string)
     for result in search_results:
